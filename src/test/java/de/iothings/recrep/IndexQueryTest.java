e77623a66bd1781ff35d15b01674d76dcf89d392
jmangold <johannes.mangold@t-online.de>
2017-11-17 15:26:03 +0100

    MultiReader Example which reads all available indexes and merges results

--- src/test/java/de/iothings/recrep/IndexQueryTest.java
+++ src/test/java/de/iothings/recrep/IndexQueryTest.java
@@ -5,6 +5,7 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.MultiReader;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.*;
@@ -15,7 +16,10 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Created by johannes on 15.11.17.
@@ -28,7 +32,7 @@ public class IndexQueryTest {
     @Test
     public void testIndexQuery() throws ParseException {
         try {
-            Optional<Path> indexPath = Files.list(FileSystems.getDefault().getPath(testRecordJobFilePath))
+            List<Path> paths = Files.list(FileSystems.getDefault().getPath(testRecordJobFilePath))
                 .filter(path -> path.toFile().isDirectory())
                 .flatMap(path -> {
                     try {
@@ -39,21 +43,28 @@ public class IndexQueryTest {
                     }
                 })
                 .filter(path -> path.toFile().isDirectory())
-                .findFirst();
+                .collect(Collectors.toList());
 
-            if (indexPath.isPresent()) {
-
-                Path path = indexPath.get();
+            if (paths != null && paths.size() > 0) {
 
                 System.out.println("Searching for '" + searchString + "' using QueryParser");
                 QueryParser queryParser = new QueryParser(RecrepIndexDocumentFields.DEFAULT_INDEX, new StandardAnalyzer());
 
                 Query query = queryParser.parse(searchString);
 
+                List<IndexReader> readers = paths.stream()
+                        .map(path -> {
+                            try {
+                                return DirectoryReader.open(FSDirectory.open(path));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                return null;
+                            }
+                        })
+                        .collect(Collectors.toList());
 
-                IndexReader reader = DirectoryReader.open(FSDirectory.open(path));
-                IndexSearcher searcher = new IndexSearcher(reader);
-
+                MultiReader multiReader = new MultiReader(readers.toArray(new IndexReader[readers.size()]));
+                IndexSearcher searcher = new IndexSearcher(multiReader);
 
                 TopDocs docs = searcher.search(query, 10);
                 ScoreDoc[] hits = docs.scoreDocs;
@@ -64,7 +75,7 @@ public class IndexQueryTest {
                     Document d = searcher.doc(docId);
                     System.out.println((i + 1) + ". " + d.get("word1") + " - " + d.get("word2") + " - " + d.get("payload"));
                 }
-                reader.close();
+                multiReader.close();
             }
 
 
